fa285bb5deac8459a1e1bb6e9ca80444962a4d86
ricardoosorioc <42077272+ricardoosorioc@users.noreply.github.com>
2025-06-27 10:22:58 -0500

    Update TestRunner.java

--- src/test/java/org/udea/parabank/TestRunner.java
+++ src/test/java/org/udea/parabank/TestRunner.java
@@ -4,12 +4,7 @@ import com.intuit.karate.junit5.Karate;
 
 class TestRunner {
 
-    @Karate.Test
-    Karate test01_ContactAppLogin() {
-        return Karate.run("login")
-                .relativeTo(getClass())
-                .outputCucumberJson(true);
-    }
+
 
     @Karate.Test
     Karate test02_ContactAppCreateContact() {
